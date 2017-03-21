c2327d7bf95471f95a7eea6cb9147d526c51e9a0
Haimo bai <haimobai.adrian@gmail.com>
2017-03-21 00:27:09 -0700

    layout

--- app/scripts/landingPage.js
+++ app/scripts/landingPage.js
@@ -1,13 +1,13 @@
 (function () {
-      var lists = ['Chloe.', 'Designer.', 'Dreamer.', 'Planner.'];
+      var lists = ['Chloe.', 'a Designer.', 'a Dreamer.', 'a Planner.'];
       var c = $('#landingText');
       var i = 0;
 
       setInterval(function () {
-        c.fadeOut(1000, function() {
+        c.fadeOut(600, function() {
           i = (i + 1) % lists.length
           c.text(lists[i])
-          c.fadeIn(1000)
+          c.fadeIn(600)
         })
-      }, 3000)
+      }, 2000)
 })()
