73cf85b1454190f8e82ffbb2da9d52c858ac1e6f
Haimo bai <haimobai.adrian@gmail.com>
2018-04-15 03:16:02 -0700

    new version of chloe website

--- app/scripts/base.js
+++ app/scripts/base.js
@@ -1,14 +1,29 @@
-/* ----------------------------------------------------------- */
-/*  WOW Animation
-/* ----------------------------------------------------------- */ 
-jQuery(function($) {
-  wow = new WOW({
-    animateClass: 'animated',
-    offset:       100,
-    live:         true,
-    callback:     function(box) {
-      console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
-    }
+// /* ----------------------------------------------------------- */
+// /*  WOW Animation
+// /* ----------------------------------------------------------- */ 
+// jQuery(function($) {
+//   wow = new WOW({
+//     animateClass: 'animated',
+//     offset:       100,
+//     live:         true,
+//     callback:     function(box) {
+//       console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+//     }
+//   });
+//   wow.init();
+// });
+$(document).ready(function() {
+  var btn = $("#menu-btn");
+  var bg = $("#about-background");
+  var close = $("#menu-close-btn");
+  btn.click( function() {
+    bg.css("display", "block");
+    btn.css("display", "none");
+    $("body").css("overflow-y", "hidden");
+  } );
+  close.click(function() {
+    bg.css("display", "none");
+    btn.css("display", "block");
+    $("body").css("overflow-y", "");
   });
-  wow.init();
 });
---
+++ app/scripts/springRole.js
@@ -0,0 +1,65 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 122,
+  pagination: {
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+});
+$(document).on('DOMNodeInserted', function(e) {
+    if ( $(e.target).hasClass('swiper-slide-active') ) {
+       //element with .MyClass was inserted.
+       console.log($(this));
+    }
+});
+// $(document).ready(function() {
+//   console.log("????")
+//   $('#s1').bind('DOMSubtreeModified', function(e) {
+//       console.log($(this));
+//       if ($(this).hasClass("swiper-slide-active")) {
+//         $(this).css("filter", "grayscale(0%)");
+//       } else {
+//         $(this).css("filter", "grayscale(100%)");
+//       }
+//   });
+//   $('#s2').bind('DOMSubtreeModified', function(e) {
+//       if ($(this).hasClass("swiper-slide-active")) {
+//         $(this).css("filter", "grayscale(0%)");
+//       } else {
+//         $(this).css("filter", "grayscale(100%)");
+//       }
+//   });
+//   $('#s3').bind('DOMSubtreeModified', function(e) {
+//       if ($(this).hasClass("swiper-slide-active")) {
+//         $(this).css("filter", "grayscale(0%)");
+//       } else {
+//         $(this).css("filter", "grayscale(100%)");
+//       }
+//   });
+//   $('#s4').bind('DOMSubtreeModified', function(e) {
+//       if ($(this).hasClass("swiper-slide-active")) {
+//         $(this).css("filter", "grayscale(0%)");
+//       } else {
+//         $(this).css("filter", "grayscale(100%)");
+//       }
+//   });
+//   $('#s5').bind('DOMSubtreeModified', function(e) {
+//       if ($(this).hasClass("swiper-slide-active")) {
+//         $(this).css("filter", "grayscale(0%)");
+//       } else {
+//         $(this).css("filter", "grayscale(100%)");
+//       }
+//   });
+//   $('#s6').bind('DOMSubtreeModified', function(e) {
+//       if ($(this).hasClass("swiper-slide-active")) {
+//         $(this).css("filter", "grayscale(0%)");
+//       } else {
+//         $(this).css("filter", "grayscale(100%)");
+//       }
+//   });
+// });
